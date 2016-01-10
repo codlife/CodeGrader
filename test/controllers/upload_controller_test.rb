65dadb36465e0f7d68b5efaafd1b650d919c34e1
codlife <1004910847@qq.com>
2016-01-10 14:46:57 +0800

    with tdd test

--- test/controllers/upload_controller_test.rb
+++ test/controllers/upload_controller_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class UploadControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  # end
+  test "should get supload" do
+  	get :supload
+    	assert_response :success
+  end
+  test "should get tupload" do
+  	get :tupload
+  	assert_response :success
   end
 
+  test "should post uploadFile"  do
+  	post :uploadFile
+  	assert_response :success	
+  end
+ 
 end
+ 
